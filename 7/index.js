78b0830b583b9f0113805ece8ddd853cfbf1192c
Berat Murathan Bayram <brtbyram.61@gmail.com>
2022-11-14 19:06:57 +0300

    7th day is over

--- 7/index.js
+++
@@ -1,8 +0,0 @@
-function setCookie(name, value, days){
-    var date = new Date();
-    date.setDate(date.getDate() + days);
-    document.cookie = name + '=' + escape(value) + '; expires=' + date.toUTCString();
-    console.log('cookie ' + name + ' created');
-}
-
-setCookie('takazlı', true, 5);
